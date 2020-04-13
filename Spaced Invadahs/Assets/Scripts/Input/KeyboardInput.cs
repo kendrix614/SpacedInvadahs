f747261fda7f2bb669733ba6dbff9492044933dc
Dela <stevedelatorre@gmail.com>
2020-04-13 09:22:47 -0400

    Keyboard Input
    and player movement

---
+++ Spaced Invadahs/Assets/Scripts/Input/KeyboardInput.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardInput : MonoBehaviour
+{
+    // variable to hold Mario sprite
+    public Player goMario;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If the player presses SPACE bar we'll make Mario Jump
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print("SPACE");
+        }
+
+        // If Move Mario right
+        if ( ( Input.GetKeyDown(KeyCode.Keypad6) ) || (Input.GetKeyDown(KeyCode.RightArrow) ) )
+        {
+            //print("Keypad6 - Right");
+            goMario.newGear = Movement.WALKING_RIGHT;
+        } else if ( (Input.GetKeyUp(KeyCode.Keypad6) ) || (Input.GetKeyUp(KeyCode.RightArrow) ) ) {
+            //print("Keypad6 Up - Right");
+            goMario.newGear = Movement.STANDING_RIGHT;
+        }
+
+        // If the player presses KeyPad4 then move Mario left
+        if ( (Input.GetKeyDown(KeyCode.Keypad4)) || (Input.GetKeyDown(KeyCode.LeftArrow)) )
+        {
+            //print("Keypad4 Down - Left");
+            goMario.newGear = Movement.WALKING_LEFT;
+        } else if ((Input.GetKeyUp(KeyCode.Keypad4)) || (Input.GetKeyUp(KeyCode.LeftArrow)) ) {
+            //print("Keypad4 Up - Left");
+            goMario.newGear = Movement.STANDING_LEFT;
+        }
+
+        // If the player presses Keypad8 then move Mario Up
+        if (Input.GetKeyDown(KeyCode.Keypad8))
+        {
+            print("Keypad8 - Up");
+        }
+
+        // If the player presses Keypad2 then make Mario Crouch
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            print("Keypad2 - Crouch or down pipe");
+        }
+    }
+}
---
+++ Spaced Invadahs/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Movement
+{
+    WALKING_RIGHT,
+    WALKING_LEFT,
+    RUNNING_RIGHT,
+    RUNNING_LEFT,
+    JUMPING_RIGHT,
+    JUMPING_LEFT,
+    STANDING_LEFT,
+    STANDING_RIGHT,
+    NOTHING
+}
+
+public class Player : MonoBehaviour
+{
+    public Sprite marioStandsLeft;
+    public Sprite marioStandsRight;
+    public Sprite marioChangeDirectionLeft;
+    public Sprite marioChangeDirectionRight;
+
+    protected int currentAnimationFrame = 0;
+    public Sprite[] marioWalkingLeft;
+    public Sprite[] marioWalkingRight;
+
+    [HideInInspector] public Movement newGear = Movement.NOTHING;
+    [HideInInspector] public Movement currentGear = Movement.NOTHING;
+
+    protected Vector3 moveRightAmount = new Vector3(.2f, 0, 0);
+    protected Vector3 moveLeftAmount = new Vector3(-.2f, 0, 0);
+
+    public void MoveRight()
+    {
+        // Check if Mario can move right
+
+        // Move Mario to the right
+        this.gameObject.transform.position += moveRightAmount;
+
+        // update marios sprite
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = marioWalkingRight[currentAnimationFrame];
+        currentAnimationFrame++;
+
+        if (currentAnimationFrame >= marioWalkingRight.Length) currentAnimationFrame = 0;
+    }
+
+    public void MoveLeft()
+    {
+        // Check if Mario can move left
+
+        // Move Mario to the left
+        this.gameObject.transform.position += moveLeftAmount;
+
+        // update marios sprite
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = marioWalkingLeft[currentAnimationFrame];
+        currentAnimationFrame++;
+
+        if (currentAnimationFrame >= marioWalkingLeft.Length) currentAnimationFrame = 0;
+    }
+
+    public void StandRight()
+    {
+        // Change Mario to stand left
+        
+    }
+
+    public void StandLeft()
+    {
+        // Change Mario to stand left
+
+    }
+
+    private float timerLength = 0.025f;
+    private float targetTime = 0.01f;
+
+    void Update()
+    {
+        //print("Update: targetTime="+ targetTime);
+        targetTime -= Time.deltaTime;
+
+        if (targetTime <= 0.0f)
+        {
+            TimerEnded();
+            targetTime = timerLength;
+        }
+
+    }
+
+    public void TimerEnded()
+    {
+        //print("TimerEnded");
+        if (newGear != currentGear)
+        {
+            // Change Mario's Sprite to a new Sprite
+            if (newGear == Movement.STANDING_LEFT)
+            {
+                print("marioChangeDirectionLeft");
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = marioChangeDirectionLeft;
+            }
+            else if (newGear == Movement.STANDING_RIGHT)
+            {
+                print("marioChangeDirectionRight");
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = marioChangeDirectionRight;
+            }
+
+            // Change Mario's previousGear to match
+            currentGear = newGear;
+        } else
+        {
+            // Keep Mario moving in his current direction
+            if (currentGear == Movement.STANDING_LEFT)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = marioStandsLeft;
+            }
+            else if (currentGear == Movement.STANDING_RIGHT)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = marioStandsRight;
+            } else if (currentGear == Movement.WALKING_LEFT)
+            {
+                MoveLeft();
+            }
+            else if (currentGear == Movement.WALKING_RIGHT)
+            {
+                MoveRight();
+            }
+        }
+    }
+}
